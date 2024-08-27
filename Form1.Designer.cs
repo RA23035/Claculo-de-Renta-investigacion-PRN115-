9d417026e6f2da774ea77a6e5db7f3106f257e5d
Roberto Rafael Ramos Alfaro <RA23035@ti.ues.edu.sv>
2024-08-26 20:30:31 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Renta_Mensual_Tarea_Ctrl.de_Versiones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxPeriodo = new System.Windows.Forms.ComboBox();
+            this.lblPeriodo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSueldo = new System.Windows.Forms.TextBox();
+            this.BtnCalcular = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.BtnCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbxPeriodo
+            // 
+            this.cbxPeriodo.FormattingEnabled = true;
+            this.cbxPeriodo.Items.AddRange(new object[] {
+            "Mensual",
+            "Quincenal",
+            "Semanal"});
+            this.cbxPeriodo.Location = new System.Drawing.Point(108, 88);
+            this.cbxPeriodo.Name = "cbxPeriodo";
+            this.cbxPeriodo.Size = new System.Drawing.Size(121, 21);
+            this.cbxPeriodo.TabIndex = 0;
+            this.cbxPeriodo.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lblPeriodo
+            // 
+            this.lblPeriodo.AutoSize = true;
+            this.lblPeriodo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeriodo.Location = new System.Drawing.Point(59, 49);
+            this.lblPeriodo.Name = "lblPeriodo";
+            this.lblPeriodo.Size = new System.Drawing.Size(206, 20);
+            this.lblPeriodo.TabIndex = 1;
+            this.lblPeriodo.Text = "Periodo de Pago de  Sueldo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre Completo";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(60, 165);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(220, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sueldo Devengado";
+            // 
+            // txtSueldo
+            // 
+            this.txtSueldo.Location = new System.Drawing.Point(63, 222);
+            this.txtSueldo.Name = "txtSueldo";
+            this.txtSueldo.Size = new System.Drawing.Size(217, 20);
+            this.txtSueldo.TabIndex = 5;
+            // 
+            // BtnCalcular
+            // 
+            this.BtnCalcular.Location = new System.Drawing.Point(41, 291);
+            this.BtnCalcular.Name = "BtnCalcular";
+            this.BtnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.BtnCalcular.TabIndex = 6;
+            this.BtnCalcular.Text = "Calcular";
+            this.BtnCalcular.UseVisualStyleBackColor = true;
+            this.BtnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(136, 291);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrar.TabIndex = 7;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // BtnCerrar
+            // 
+            this.BtnCerrar.Location = new System.Drawing.Point(229, 291);
+            this.BtnCerrar.Name = "BtnCerrar";
+            this.BtnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.BtnCerrar.TabIndex = 8;
+            this.BtnCerrar.Text = "Cerrar";
+            this.BtnCerrar.UseVisualStyleBackColor = true;
+            this.BtnCerrar.Click += new System.EventHandler(this.BtnCerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 355);
+            this.Controls.Add(this.BtnCerrar);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.BtnCalcular);
+            this.Controls.Add(this.txtSueldo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPeriodo);
+            this.Controls.Add(this.cbxPeriodo);
+            this.Name = "Form1";
+            this.Text = "Calculo de Renta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxPeriodo;
+        private System.Windows.Forms.Label lblPeriodo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSueldo;
+        private System.Windows.Forms.Button BtnCalcular;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button BtnCerrar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Renta_Mensual_Tarea_Ctrl.de_Versiones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtNombre.Clear();
+            txtSueldo.Clear();
+            
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            string Nombre = txtNombre.Text;
+            Double Sueldo, Renta=0.00;
+           int seleccion = cbxPeriodo.SelectedIndex;
+
+            switch (seleccion)
+            {
+                case 0:
+                    Sueldo = Int32.Parse(txtSueldo.Text);
+                    if (Sueldo >= 2058.68)
+                    {
+                        Renta = (Sueldo - 2058.67) * 0.3 + 288.57;
+                        MessageBox.Show(Renta.ToString(),Nombre);
+                    }
+                    else if (Sueldo >= 487.61 && Sueldo <= 642.85)
+                    {
+                        Renta = (Sueldo - 487.60) * 0.1 + 17.48;
+                        MessageBox.Show(Renta.ToString(), Nombre);
+                    }
+                    else if (Sueldo >= 642.86 && Sueldo <= 915.81)
+                    {
+                        Renta = (Sueldo - 642.85) * 0.1 + 32.70;
+                        MessageBox.Show(Renta.ToString(), Nombre);
+                    }
+                    else if (Sueldo >= 915.82 && Sueldo <= 2058.68)
+                    {
+                        Renta = (Sueldo - 915.81) * 0.2 + 60.00;
+                        MessageBox.Show(Renta.ToString(), Nombre);
+                    }
+                    else
+                    {
+                        Renta = 0.00;
+                        MessageBox.Show(Nombre + Renta.ToString());
+                    }
+                    break;
+                case 1:
+                    MessageBox.Show("Esta tabla solo calcula Sueldos pagaderos Mensualmente");
+                    break;
+                case 2:
+                    MessageBox.Show("Esta tabla solo calcula Sueldos pagaderos Mensualmente");
+                    break;            
+                default:
+                    MessageBox.Show("Por favor Click en boton borrar y Seleccione el Periodo de Pago");
+                    break;
+
+            }
+
+        }
+    }
+}
